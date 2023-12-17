caabd22b537b65a6a7820bc1adbecaab546954a4
ndpmmo <49186991+phongcongtu@users.noreply.github.com>
2023-12-17 21:10:58 +0700

    up

---
+++ api/privacy.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+     
+    <title>Privacy policy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link href="https://fonts.googleapis.com/css?family=Oxygen:300,400" rel="stylesheet" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700"
+      rel="stylesheet"
+    />
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css" />
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css" />
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css" />
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css" />
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+  </head>
+  <body>
+    <div class="fh5co-loader"></div>
+
+    <div id="page">
+      <nav class="fh5co-nav" role="navigation">
+        <div class="container-wrap">
+          <div class="top-menu">
+            <div class="row">
+              <div class="col-xs-2">
+                <div id="fh5co-logo">
+                  <a href="index.php" style="font-size: 10px">Crowdsourcing</a>
+                </div>
+              </div>
+              <div class="col-xs-10 text-right menu-1">
+                <ul>
+                  <li><a href="index.php">Main</a></li>
+                  <li><a href="blog.php">Articles</a></li>
+                  <li><a href="contact.php">Contacts</a></li>
+                  <li class="active"><a href="privacy.php">Privacy policy</a></li>
+                  <li><a href="terms.php">Terms and conditions</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="container-wrap">
+        <aside id="fh5co-hero">
+          <div class="flexslider">
+            <ul class="slides">
+              <li style="background-image: url('./assets/images/bulksplash-szolkin-_uey8ati6d0.jpg')">
+                <div class="overlay-gradient"></div>
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-6 col-md-offset-3 col-md-pull-3 slider-text">
+                      <div class="slider-text-inner">
+                        <h1 style="color: #fff">Privacy policy</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </aside>
+
+        <div id="fh5co-work" class="fh5co-light-grey">
+          <section class="text-center">
+            <h1>Privacy Policy for <span class="server-name"></span></h1>
+
+            <p>
+              At <span class="server-name"></span>, accessible from
+              <span class="server-name"></span>, one of our main priorities is the privacy of our
+              visitors. This Privacy Policy document contains types of information that is collected
+              and recorded by <span class="server-name"></span> and how we use it.
+            </p>
+
+            <p>
+              If you have additional questions or require more information about our Privacy Policy,
+              do not hesitate to contact us.
+            </p>
+
+            <h2>Log Files</h2>
+
+            <p>
+              <span class="server-name"></span> follows a standard procedure of using log files.
+              These files log visitors when they visit websites. All hosting companies do this and a
+              part of hosting services' analytics. The information collected by log files include
+              internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date
+              and time stamp, referring/exit pages, and possibly the number of clicks. These are not
+              linked to any information that is personally identifiable. The purpose of the
+              information is for analyzing trends, administering the site, tracking users' movement
+              on the website, and gathering demographic information. Our Privacy Policy was created
+              with the help of the
+              <a href="https://www.privacypolicyonline.com/privacy-policy-generator/"
+                >Privacy Policy Generator</a
+              >.
+            </p>
+
+            <h2>Cookies and Web Beacons</h2>
+
+            <p>
+              Like any other website, <span class="server-name"></span> uses 'cookies'. These
+              cookies are used to store information including visitors' preferences, and the pages
+              on the website that the visitor accessed or visited. The information is used to
+              optimize the users' experience by customizing our web page content based on visitors'
+              browser type and/or other information.
+            </p>
+
+            <p>
+              For more general information on cookies, please read
+              <a href="https://www.privacypolicyonline.com/what-are-cookies/"
+                >the "Cookies" article from the Privacy Policy Generator</a
+              >.
+            </p>
+
+            <h2>Privacy Policies</h2>
+
+            <p>
+              You may consult this list to find the Privacy Policy for each of the advertising
+              partners of
+              <span class="server-name"></span>.
+            </p>
+
+            <p>
+              Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or
+              Web Beacons that are used in their respective advertisements and links that appear on
+              <span class="server-name"></span>, which are sent directly to users' browser. They
+              automatically receive your IP address when this occurs. These technologies are used to
+              measure the effectiveness of their advertising campaigns and/or to personalize the
+              advertising content that you see on websites that you visit.
+            </p>
+
+            <p>
+              Note that <span class="server-name"></span> has no access to or control over these
+              cookies that are used by third-party advertisers.
+            </p>
+
+            <h2>Third Party Privacy Policies</h2>
+
+            <p>
+              <span class="server-name"></span>'s Privacy Policy does not apply to other advertisers
+              or websites. Thus, we are advising you to consult the respective Privacy Policies of
+              these third-party ad servers for more detailed information. It may include their
+              practices and instructions about how to opt-out of certain options.
+            </p>
+
+            <p>
+              You can choose to disable cookies through your individual browser options. To know
+              more detailed information about cookie management with specific web browsers, it can
+              be found at the browsers' respective websites. What Are Cookies?
+            </p>
+
+            <h2>Children's Information</h2>
+
+            <p>
+              Another part of our priority is adding protection for children while using the
+              internet. We encourage parents and guardians to observe, participate in, and/or
+              monitor and guide their online activity.
+            </p>
+
+            <p>
+              <span class="server-name"></span> does not knowingly collect any Personal Identifiable
+              Information from children under the age of 13. If you think that your child provided
+              this kind of information on our website, we strongly encourage you to contact us
+              immediately and we will do our best efforts to promptly remove such information from
+              our records.
+            </p>
+
+            <h2>Online Privacy Policy Only</h2>
+
+            <p>
+              This Privacy Policy applies only to our online activities and is valid for visitors to
+              our website with regards to the information that they shared and/or collect in
+              <span class="server-name"></span>. This policy is not applicable to any information
+              collected offline or via channels other than this website.
+            </p>
+
+            <h2>Consent</h2>
+
+            <p>
+              By using our website, you hereby consent to our Privacy Policy and agree to its Terms
+              and Conditions.
+            </p>
+          </section>
+        </div>
+      </div>
+      <!-- END container-wrap -->
+
+      <div class="container-wrap">
+        <footer id="fh5co-footer" role="contentinfo">
+          <div class="row copyright">
+            <div class="col-md-12 text-center">
+              <p>
+                <small class="block">
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  Copyright</small
+                >
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+      <!-- END container-wrap -->
+    </div>
+
+    <div class="gototop js-top">
+      <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+    </div>
+
+    <div class="cookie-banner">
+      <p style="color: #000000">
+        The site uses cookies. They allow us to recognize you and get information about your user experience.By continuing to browse the site, I agree to the use of cookies by the site owner in accordance with 
+        <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie"
+          >Cookie policy</a
+        >
+      </p>
+
+      <button class="close-cookie">×</button>
+    </div>
+
+    <script>
+      window.onload = function () {
+        $('.close-cookie').click(function () {
+          $('.cookie-banner').fadeOut();
+        });
+      };
+    </script>
+
+    <script>
+      let elems = document.querySelectorAll('.server-name');
+      elems.forEach((elem) => {
+        elem.innerHTML = window.location.hostname;
+      });
+    </script>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    <!-- Counters -->
+    <script src="js/jquery.countTo.js"></script>
+    <!-- Main -->
+    <script src="js/main.js"></script>
+  </body>
+</html>
